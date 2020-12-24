02a649ed20a8207859cd22bd5f737d2fa721a5d2
cumhurcan çankaya <cumhurcangs97@gmail.com>
2020-12-24 23:50:53 +0300

    Added migration,sql connection

--- Shop.UI/Startup.cs
+++ Shop.UI/Startup.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+
+
 
 namespace Shop.UI
 {
@@ -15,15 +20,16 @@ namespace Shop.UI
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _config = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration _config { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(_config["DefaultConnection"],b=>b.MigrationsAssembly("Shop.UI")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
